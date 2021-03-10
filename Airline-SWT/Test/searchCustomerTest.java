12d4a2d74f902514ee7f8d5e9f107a1ba5236a1c
Rebbica007 <jrgrunewald5184@eagle.fgcu.edu>
2021-03-10 14:31:06 -0500

    Trying to test the browse button, but it is not working yet.

--- Airline-SWT/Test/searchCustomerTest.java
+++ Airline-SWT/Test/searchCustomerTest.java
@@ -9,6 +9,40 @@ import static org.junit.Assert.fail;
 // This is for negative testing (exception testing)
 public class searchCustomerTest {
 
+  // NOT working yet
+  // Trying to set the txtPhoto label to null but I think
+  // we may need to make the byte[] userimage null or the
+  // String path null to test it. Not sure what I am doing to be honest.
+  @Test
+  public void testButton1SQLException() throws InterruptedException {
+    // Create instance of the searchCustomer class
+    searchCustomer search = new searchCustomer();
+    search.setVisible(true);
+    JTextField findText = (JTextField) search.getFindText();
+    if (findText.getVisibleRect().isEmpty()){
+      findText.setText("CS001");
+    }
+    else {
+      findText.getText();
+    }
+    // Access the label used to display image
+    JLabel photo = (JLabel) search.getFrameIcon();
+    // Enter an invalid input (no image path) to trigger SQLException
+    photo.setIcon(null);
+    // Give program 2 seconds to accept text
+    sleep(2000);
+    // Click button1 (Browse) to attempt a search with the invalid input
+    searchCustomer.jButton1.doClick();
+    // In searchCustomer SQLException catch, button1IsSQLThrown set to true
+    // This was work around since I wasn't able to get
+    // jButton1ActionPerformed to throw an exception for some reason
+    // If invalid input, and SQL not thrown, test fails
+    // If invalid input, and SQL thrown, test passes
+    Assert.assertTrue(searchCustomer.button1IsSQLThrown);
+    System.out.println("Browse Button SQL exception was caught");
+
+  }
+
 
 
   // To produce exception we need to input letters (ABC) into the contact,
